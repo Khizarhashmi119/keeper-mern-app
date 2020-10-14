ce44c5bf5bb75d4a9c291afeafe64f10ade1d494
khizarhashmi119 <khizarhashmi119@gmail.com>
2020-10-14 23:10:41 +0530

    add deployment config.

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+if(process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const connectDB = require("./db");
 const noteRoutes = require("./routes/api/note-routes");
@@ -14,5 +18,13 @@ app.use(express.json({ extended: true }));
 app.use(cors());
 
 app.use("/api", noteRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}...`));
